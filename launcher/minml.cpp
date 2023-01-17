4be927e75764054e49594ccfcd7d0bc8761842a4
Maciej Kozarzewski <mk@eskrzynka.pl>
2023-01-17 03:07:33 +0100

    Implemented base software fallback for fp16 conversion.

--- launcher/minml.cpp
+++ launcher/minml.cpp
@@ -22,6 +22,8 @@
 #include <iostream>
 #include <fstream>
 #include <memory>
+#include <cmath>
+#include <x86intrin.h>
 
 using namespace ml;
 
@@ -141,9 +143,335 @@ double get_accuracy(const Tensor &output, const Tensor &target)
 	return correct;
 }
 
+uint32_t as_uint(const float x)
+{
+	return *(uint32_t*) &x;
+}
+float as_float(const uint32_t x)
+{
+	return *(float*) &x;
+}
+
+void print_bits(const uint16_t x)
+{
+	for (int i = 15; i >= 0; i--)
+	{
+		std::cout << ((x >> i) & 1);
+		if (i == 15 || i == 10)
+			std::cout << " ";
+	}
+	std::cout << '\n';
+}
+void print_bits(const uint32_t x)
+{
+	for (int i = 31; i >= 0; i--)
+		std::cout << ((x >> i) & 1);
+	std::cout << '\n';
+}
+void print_bits(const float x)
+{
+	uint32_t b = *(uint32_t*) &x;
+	for (int i = 31; i >= 0; i--)
+	{
+		std::cout << ((b >> i) & 1);
+		if (i == 31 or i == 23)
+			std::cout << " ";
+	}
+	int e = (b & 0x7F800000u) >> 23u;
+	std::cout << " : exponent = " << e << " (" << e - 127 << "), mantissa = " << (b & 0x007FFFFFu) << '\n';
+}
+
+// IEEE-754 16-bit floating-point format
+float half_to_float(const uint16_t x)
+{
+//	uint32_t exponent = (x & 0x7C00) >> 10; // exponent
+//	uint32_t mantissa = (x & 0x03FF) << 13; // mantissa
+//
+//	const uint32_t sign = (x & 0x8000) << 16;
+//	if (exponent == 31)
+//	{
+//		exponent = 0x7F800000; // +/- Inf or +/- NaN
+//		if (mantissa != 0)
+//			mantissa |= (1 << 22);
+//	}
+//	else
+//	{
+//		if (exponent != 0)
+//			exponent = (exponent + 112) << 23; // normalized
+//		else
+//		{
+//			if (exponent == 0 and mantissa != 0)
+//			{ // denormalized
+//				const uint32_t v = _lzcnt_u32(mantissa);
+//				exponent = (121 - v) << 23;
+//				mantissa = (mantissa << (v - 8)) & 0x007FE000;
+//			}
+//		}
+//	}
+	uint32_t exponent = x & 0x7C00; // '0 11111 0000000000'
+	uint32_t mantissa = x & 0x03FF; // '0 00000 1111111111'
+
+	const uint32_t sign = (x & 0x8000) << 16; // '1 00000 0000000000'
+	if (exponent == 0x7C00)
+	{
+		exponent = 0x3FC00; // +/- Inf or +/- NaN (it's 0x7F800000 >> 13)
+		mantissa |= ((mantissa != 0) << 9); // set first bit of the mantissa in case of NaN, but preserve other bits
+	}
+	else
+	{
+		if (exponent != 0) // normalized
+			exponent += (112 << 10);
+		else
+		{
+			if (mantissa != 0)
+			{ // denormalized
+				const uint32_t v = _lzcnt_u32(mantissa);
+				exponent = (134 - v) << 10;
+				mantissa = (mantissa << (v - 21)) & 0x000003FF;
+			}
+		}
+	}
+	return as_float(sign | ((exponent | mantissa) << 13));
+
+//	const uint32_t e = (x & 0x7C00) >> 10; // exponent
+//	const uint32_t m = (x & 0x03FF) << 13; // mantissa
+//
+//	const uint32_t sign = (x & 0x8000) << 16;
+//	if (e > 30)
+//		return as_float(sign | 0x7F800000 | m); // +/- Inf or +/- NaN
+//	if (e != 0)
+//		return as_float(sign | ((e + 112) << 23 | m)); // normalized
+//	if (e == 0 and m != 0)
+//	{ // denormalized
+////		std::cout << "m = ";
+////		print_bits(m);
+////		std::cout << "    ";
+////		print_bits(as_uint((float) m));
+////		const uint32_t v = as_uint((float) m) >> 23; // evil log2 bit hack to count leading zeros in denormalized format
+////		std::cout << "v = " << v << '\n';
+////		std::cout << "lzcnt = " << _lzcnt_u32(m) << '\n';
+////		std::cout << "e = ";
+////		print_bits((v - 37) << 23);
+////		std::cout << "e'= ";
+////		print_bits((121 - _lzcnt_u32(m)) << 23);
+////		std::cout << "    ";
+////		print_bits(m << (150 - v));
+////		std::cout << "m'= ";
+////		print_bits(m << (_lzcnt_u32(m) - 8)); // new
+////		std::cout << "    ";
+////		print_bits((uint32_t) 0x007FE000);
+////		return as_float(sign | ((v - 37) << 23 | ((m << (150 - v)) & 0x007FE000)));
+//
+//		std::cout << "m = ";
+//		print_bits(m);
+//
+//		std::cout << "v = ";
+//		print_bits(as_uint((float) m));
+//		std::cout << "v = " << as_uint((float) m) << '\n';
+//
+//		const uint32_t v = _lzcnt_u32(m);
+//		std::cout << "v'= ";
+//		print_bits(v);
+//		std::cout << "v'= " << _lzcnt_u32(m) << '\n';
+//
+//		std::cout << "e = ";
+//		print_bits((v - 37) << 23);
+//		std::cout << "e'= ";
+//		print_bits((121 - _lzcnt_u32(m)) << 23);
+//		std::cout << "    ";
+//		print_bits(m << (150 - v));
+//		std::cout << "m'= ";
+//		print_bits(m << (_lzcnt_u32(m) - 8)); // new
+//		std::cout << "    ";
+//		print_bits((uint32_t) 0x007FE000);
+//		return as_float(sign | ((121 - v) << 23 | ((m << (v - 8)) & 0x007FE000)));
+//	}
+//	return as_float(sign);
+}
+// IEEE-754 16-bit floating-point format
+enum class RoundingMode
+{
+	ROUND_TO_NEAREST_INT, // round to nearest
+	ROUND_TO_NEG_INF,    // round down
+	ROUND_TO_POS_INF,   // round up
+	ROUND_TO_ZERO,  // truncate
+};
+template<RoundingMode RM = RoundingMode::ROUND_TO_NEAREST_INT>
+uint16_t float_to_half(const float x)
+{
+	const uint32_t original = as_uint(x);
+	const uint32_t rounded = original + 0x00001000; // round-to-nearest-even: add last bit after truncated mantissa
+	uint32_t exponent = (rounded & 0x7F800000) >> 23; // exponent
+	uint32_t mantissa = rounded & 0x007FFFFF; // mantissa; in line below: 0x007FF000 = 0x00800000-0x00001000 = decimal indicator flag - initial rounding
+
+	const uint32_t sign = (original & 0x80000000) >> 16;
+
+	if ((original & 0x7FFFFFFF) > 0x7F800000)
+	{ // check NaN
+		exponent = 0x7C00;
+		mantissa = ((original & 0x007FFFFF) >> 13) | 0x200; // set first mantissa bit but preserve others
+	}
+	else
+	{
+		if (exponent > 142)
+		{ // +/- Inf
+			exponent = 0x7C00;
+			mantissa = 0;
+		}
+		else
+		{
+			if (exponent > 112)
+			{ // normalized
+				exponent = ((exponent - 112) << 10) & 0x7C00;
+				mantissa >>= 13;
+			}
+			else
+			{ // denormalized
+				mantissa += 0x007FF000; // TODO figure out why it is here
+				mantissa >>= std::min(125u - exponent, 31u);
+				mantissa = (mantissa + 1) >> 1;
+				exponent = 0;
+			}
+		}
+	}
+	return sign | exponent | mantissa;
+
+//	const uint32_t b = as_uint(x); // + 0x00001000u; // round-to-nearest-even: add last bit after truncated mantissa
+//	const uint32_t e = (b & 0x7F800000u) >> 23u; // exponent
+//	const uint32_t m = b & 0x007FFFFFu; // mantissa; in line below: 0x007FF000 = 0x00800000-0x00001000 = decimal indicator flag - initial rounding
+//
+//	const uint32_t sign = (b & 0x80000000u) >> 16u;
+//
+//	if (e > 142)
+//	{
+//		if (e != 255)
+//			m = 0;
+//		return sign | 0x7c00 | (m >> 13); // +/- Inf or +/- NaN
+//	}
+//	if (e > 112)
+//	{ // normalized
+//		const uint32_t shifted_exponent = (e - 112) << 10;
+//		return sign | (shifted_exponent & 0x7C00) | (m >> 13);
+//	}
+//	if (101 < e and e < 113)
+//	{ // denormalized
+//		uint32_t tmp = 0x007FF000 + m;
+//		tmp = tmp >> (125 - e);
+//		tmp = tmp + 1;
+//		tmp = tmp >> 1;
+//		return sign | tmp;
+//	}
+//	return sign; // +/- 0
+}
+
 void test_mnist()
 {
-	std::cout << Device::hardwareInfo();
+//	std::cout << Device::hardwareInfo();
+
+//	for (int i = 0; i < 65536; i++)
+//	{
+//		const uint32_t x_native_fp32 = as_uint(_cvtsh_ss(i));
+//		const uint32_t x_emulated_fp32 = as_uint(half_to_float(i));
+//		if (x_native_fp32 != x_emulated_fp32)
+//		{
+//			std::cout << "mismatch at i = " << i << '\n';
+//			const float x_native_fp32 = _cvtsh_ss(i);
+//			const float x_emulated_fp32 = half_to_float(i);
+//
+//			std::cout << "bits (fp16)     = ";
+//			print_bits((uint16_t) i);
+//			std::cout << "bits (native)   = ";
+//			print_bits(x_native_fp32);
+//			std::cout << "bits (emulated) = ";
+//			print_bits(x_emulated_fp32);
+//
+//			std::cout << "recovered " << x_native_fp32 << " " << x_emulated_fp32 << '\n';
+//			return;
+//		}
+//	}
+//	std::cout << "all correct\n";
+
+	int64_t last_bit_mismatch = 0;
+	double start = getTime();
+//	for (int64_t i = 0; i < 4294967296; i++)
+	for (int64_t i = 0; i < 1073741824; i++)
+	{
+//		last_bit_mismatch += _cvtsh_ss(i & 65535);
+		last_bit_mismatch += half_to_float(i & 65535);
+
+//		const float x = as_float(i);
+//		last_bit_mismatch += _cvtss_sh(x, _MM_FROUND_TO_NEAREST_INT);
+//		last_bit_mismatch += float_to_half(x);
+	}
+	double stop = getTime();
+	std::cout << 4294967296.0 / (stop - start) << '\n';
+	std::cout << last_bit_mismatch << '\n';
+	return;
+
+	for (int64_t i = 0; i < 4294967296; i++)
+//	int64_t i = 2139095041;
+	{
+		if (i % 100000000 == 0)
+			std::cout << "checked " << i / 1000000 << "M ...\n";
+		const float x = as_float(i);
+		const uint16_t x_native_fp16 = _cvtss_sh(x, _MM_FROUND_TO_NEAREST_INT);
+		const uint16_t x_emulated_fp16 = float_to_half(x);
+		if (x_native_fp16 != x_emulated_fp16)
+		{
+			if ((x_native_fp16 & 0xFFFE) != (x_emulated_fp16 & 0xFFFE))
+			{
+				std::cout << "mismatch at i = " << i << '\n';
+
+				std::cout << "float = " << x << '\n';
+				std::cout << "bits (fp32)     = ";
+				print_bits((uint32_t) i);
+				std::cout << "bits (native)   = ";
+				print_bits(x_native_fp16);
+				std::cout << "bits (emulated) = ";
+				print_bits(x_emulated_fp16);
+
+				std::cout << "recovered fp32 " << _cvtsh_ss(x_native_fp16) << " " << _cvtsh_ss(x_emulated_fp16) << '\n';
+				return;
+			}
+			else
+				last_bit_mismatch++;
+		}
+	}
+	std::cout << "last bit mismatched " << last_bit_mismatch << '\n';
+	std::cout << "all correct\n";
+	return;
+
+	{
+		const float x = 1.23e-6;
+		print_bits(x);
+		const uint16_t x_native_fp16 = _cvtss_sh(x, _MM_FROUND_TO_NEAREST_INT | _MM_FROUND_NO_EXC);
+		const uint16_t x_emulated_fp16 = float_to_half(x);
+
+		std::cout << "original  " << x << '\n';
+		std::cout << "bits (native)   = ";
+		print_bits(x_native_fp16);
+		std::cout << "bits (emulated) = ";
+		print_bits(x_emulated_fp16);
+
+		std::cout << _cvtsh_ss(x_native_fp16) << " " << _cvtsh_ss(x_emulated_fp16) << '\n';
+
+		const float x_native_fp32 = _cvtsh_ss(x_native_fp16);
+		const float x_emulated_fp32 = half_to_float(x_native_fp16);
+
+		std::cout << "original  " << x << '\n';
+		std::cout << "bits (native)   = ";
+		print_bits(x_native_fp32);
+		std::cout << "bits (emulated) = ";
+		print_bits(x_emulated_fp32);
+
+		std::cout << "recovered " << x_native_fp32 << " " << x_emulated_fp32 << '\n';
+
+//		const float x_sw_fp32 = half_to_float(x_sw_fp16);
+//		const float x_hw_fp32 = _cvtsh_ss(x_hw_fp16);
+//
+
+//		std::cout << "recovered " << x_decompressed << '\n';
+	}
 	return;
 
 	Device::setNumberOfThreads(1);
